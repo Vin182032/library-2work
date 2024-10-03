70202d184051ea2ea94f1fd13a996baefd53f47e
root <root@localhost>
2024-10-03 23:18:49 +0300

    auto publish 1727986729222

---
+++ src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+// calcution of fine amount 
+int book id;
+int due date;
+int return date;
+int day over dues;
+int fine date;
+int fine amount;
+
+printf("enter the book id");
+scanf("%d",book id);
+
+printf(" enterthe due date ");
+scanf("%d",due date);
+
+printf("enter return date");
+scanf("%d", return date);
+
+printf ("enter day overdue");
+scanf("%d",day overdue);
+
+printf("enter fine rate ");
+scanf("%d",fine rate);
+
+printf ("enter fine amount");
+scanf("%d",fine amount);
+
+return 0;
+
+}
